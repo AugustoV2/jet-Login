4605404d9163c4bf87f4d7228e96ea80e09a77a5
AugustoV2 <augustovarghese@gmail.com>
2024-03-22 00:13:58 +0530

    Login page / sign up page for android

--- app/src/main/java/com/example/login/ui/theme/login/screenlogin.kt
+++ app/src/main/java/com/example/login/ui/theme/login/screenlogin.kt
@@ -13,19 +13,24 @@ import com.example.login.ui.theme.components.passwrodtxtfield
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.sp
 import com.example.login.ui.theme.components.loginbutton
+import com.example.login.ui.theme.signup.signtxt
 
 
 @Composable
-    fun LoginScreen() {
+    fun LoginScreen(){
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var check by remember { mutableStateOf(false) }
+    var enabled by rememberSaveable{ mutableStateOf(true)}
+   // val qn = AnnotatedString("Dont have an acc? Sign UP!")
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -83,9 +88,16 @@ import com.example.login.ui.theme.components.loginbutton
             ) {
                 loginbutton(username ,password ,check)
             }
+            Row{
+                signtxt()
+
+            }
         }
     }
 }
+
+
+
 
 
 
